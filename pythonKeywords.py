36666c22bacdd80847fd6c8f9cfd743e0b02224d
Abhishek <abhisingh8734@gmail.com>
2025-06-09 14:47:48 +0530

    Keyword practice

--- pythonKeywords.py
+++ pythonKeywords.py
@@ -1,6 +1,27 @@
 #initial commit
 
 #Printing all keyword once using kwlist()
-import keyword
-print("The list of all keywords in pythons is: ")
-print(keyword.kwlist)
+# import keyword
+# print("The list of all keywords in pythons is: ")
+# print(keyword.kwlist)
+
+#Value Keywords: True, False, None Keyword, del
+# print(False == 0)
+# print(True == 1)
+# In python booleans have their own integer value as in True=1, False=0
+# print(True + True + True)#Ans:3
+# print(True + False + False)#Ans: 1
+# print(None == 0)
+# print(None == [])
+
+a= True
+b= False
+print(a and b)
+print(a or b)
+print(not b)
+
+print(3 in [1,2,3])
+if 's' in "Abhishek":
+    print("S is present in your name")
+else:
+    print("S is not present in your name.")
