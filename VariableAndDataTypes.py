feadf3809910e783021c177d27d2a9a86a4278cd
Abhishek <abhisingh8734@gmail.com>
2025-07-03 12:47:32 +0530

    About Variable And Data Types in Python

---
+++ VariableAndDataTypes.py
@@ -0,0 +1,12 @@
+#TITILE: Variable and Data Types
+#Variable: Variable is like a container that holds data
+#Data Type: Data type specifies the type of value a variable holds
+#In Pyhton everything is Object
+a=1
+b=True
+c="Abhishek"
+d=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
