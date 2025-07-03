895844d4e2570390e2af0a2b08ede4c94d1f2476
Abhishek <abhisingh8734@gmail.com>
2025-07-03 12:23:11 +0530

    Practice First

---
+++ prac1.py
@@ -0,0 +1,9 @@
+#TITLE : Comments, Escape Sequence and Print statements in Python
+
+# This is Single line comment
+#\n : Use for new line
+'''
+This is multiline comment
+'''
+print("My name is \"Abhishek\" \nand i am learning Python")
+print("Hey Abhishek",sep=":")
